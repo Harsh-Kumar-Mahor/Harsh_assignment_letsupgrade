71a9e38a3aab8ef9df2cceb006958e5feafc6d16
HARSH KUMAR MAHOR <109431395+Harsh-Kumar-Mahor@users.noreply.github.com>
2022-10-07 00:18:19 +0530

    Add files via upload

---
+++ Harsh_assignment_day3.py
@@ -0,0 +1,8 @@
+#this program checks whether a number is even or odd
+num=int(input("Enter the number to check whether it's (even/odd): "))
+#if number is even then the remainder of  division of number with 2 is 0
+if num%2==0:
+    print("The number is even")
+#otherwise print odd
+else:
+    print("The number is odd")
